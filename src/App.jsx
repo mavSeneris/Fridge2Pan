555ec36aad9ca390a2b6ddd9b5ef8d822e20d12b
choMatt <mattseneris18@gmail.com>
2023-07-02 14:42:02 +0800

    imported Fridge component and rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Fridge from './components/Fridge'
 
 function App() {
   return (
-    <>
-    </>
+    <main>
+      <Fridge />
+    </main >
   )
 }
 
