438f4e18655bc02c57e9fdf6dc79ac7f09b9130f
choMatt <mattseneris18@gmail.com>
2023-07-03 19:23:12 +0800

    imported Search to App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import "./App.css";
 import Fridge from "./pages/Fridge";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 const route = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Layout/>}>
