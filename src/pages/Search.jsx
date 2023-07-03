419ba8fc5db896421d70137553668d99c4fdcf2b
choMatt <mattseneris18@gmail.com>
2023-07-03 19:22:17 +0800

    added search recipe component

---
+++ src/pages/Search.jsx
@@ -0,0 +1,25 @@
+import React  from "react";
+
+export default function Search(){
+    const [inputVal, setInputVal] = useState('')
+
+    function handleInputUpdate(event){
+        const userInput = event.target.value
+        setInputVal(userInput)
+    }
+
+    return (
+        <div className="search">
+            <div className="search__header">
+                <h2 className="search__title"> What do you want to cook?</h2>
+                <form action="">
+                    <input 
+                        type="text" 
+                        onChange={handleInputUpdate}
+                    />
+                    <button>Search</button>
+                </form>
+            </div>
+        </div>
+    )
+}
