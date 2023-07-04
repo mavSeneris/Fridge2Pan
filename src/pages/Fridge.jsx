61bdb7cca8105162f57be9c21f28e766dd45720b
Maverick <mavseneris@gmail.com>
2023-07-05 06:00:54 +0800

    added api call and added styles to site wide buttons

--- src/pages/Fridge.jsx
+++ src/pages/Fridge.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Fridge() {
   const [inputVal, setInputVal] = useState("");
   const [items, setItems] = useState([]);
+  const [response, setResponse] = useState("");
 
   function handleInputUpdate(event) {
     const rawInput = event.target.value;
@@ -11,7 +12,7 @@ export default function Fridge() {
     setInputVal(formattedInput);
   }
 
-  function addItem(event) {
+  async function addItem(event) {
     event.preventDefault();
     if (!inputVal) {
       alert("Input required");
@@ -42,7 +43,35 @@ export default function Fridge() {
     }
   };
 
-  console.log(`What can I cook with ${items.join(", ")} only.`);
+  useEffect(() => {
+    const fetchResponse = async () => {
+      const response = await fetchChatGPTResponse(items);
+      setResponse(response);
+    };
+
+    fetchResponse();
+  }, [items]);
+
+  const fetchChatGPTResponse = async (items) => {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer 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", // Replace with your actual API key
+      },
+      body: JSON.stringify({
+        messages: [
+          { role: "system", content: "You: What can I cook with " + items.join(", ") + " only." },
+          { role: "user", content: "Suggest recipes" },
+        ],
+        model: "text-davinci-003", 
+      }),
+    });
+
+    const data = await response.json();
+    const message = data.choices[0]?.message?.content || "";
+    return message.replace("AI:", "").trim();
+  };
 
   const fridgeItems = items.map((item) => (
     <div key={item} className="fridge__items">
@@ -55,41 +84,41 @@ export default function Fridge() {
 
   return (
     <section>
-
-        <div className="fridge">
+      <div className="fridge">
         <div className="fridge__scroll-wrapper">
-         <div className="fridge__list">{fridgeItems}</div>
+          <div className="fridge__list">{fridgeItems}</div>
         </div>
-          <form action="" className="fridge__form">
-
-            <div className="fridge__controls">
-              <input
-                type="text"
-                placeholder="What's in your fridge?"
-                value={inputVal}
-                onChange={handleInputUpdate}
-                onKeyDown={handleKeyDown}
-                id="input-field"
-                className="fridge__input"
-              />
-              <button
-                type="button"
-                onClick={addItem}
-                className="fridge__button fridge__button--add"
-              >
-                Add
-              </button>
-              <button
-                type="button"
-                onClick={clearAll}
-                className="fridge__button fridge__button--clear"
-              >
-                Clear
-              </button>
-            </div>
-           
-          </form>
-        </div>
+        <form action="" className="fridge__form">
+          <div className="fridge__controls">
+            <input
+              type="text"
+              placeholder="What's in your fridge?"
+              value={inputVal}
+              onChange={handleInputUpdate}
+              onKeyDown={handleKeyDown}
+              id="input-field"
+              className="fridge__input"
+            />
+            <button
+              type="button"
+              onClick={addItem}
+              className="fridge__button fridge__button--add"
+            >
+              Add
+            </button>
+            <button
+              type="button"
+              onClick={clearAll}
+              className="fridge__button fridge__button--clear"
+            >
+              Clear
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="response">
+        <p>{response}</p>
+      </div>
     </section>
   );
 }
