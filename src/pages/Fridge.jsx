6734d644cfab70ccda5e849a1a2a9a00c68c7c32
Maverick <mavseneris@gmail.com>
2023-07-07 08:13:35 +0800

    added card styling to fridge scroll wrapper and added org keys in Fridge page

--- src/pages/Fridge.jsx
+++ src/pages/Fridge.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import EmptyState from "../components/EmptyState"
-import DeleteIcon from "../assets/DeleteIcon.svg"
-
+import EmptyState from "../components/EmptyState";
+import DeleteIcon from "../assets/DeleteIcon.svg";
 
 export default function Fridge() {
   const [inputVal, setInputVal] = useState("");
   const [items, setItems] = useState([]);
   const [response, setResponse] = useState("");
 
-  console.log('page rendered')
+  console.log("page rendered");
   function handleInputUpdate(event) {
     const rawInput = event.target.value;
     const formattedInput =
@@ -30,7 +29,7 @@ export default function Fridge() {
     }
   }
 
-  function submit(){
+  function submit() {
     fetchChatGPTResponse(items);
   }
 
@@ -43,7 +42,6 @@ export default function Fridge() {
     setItems([]);
   }
 
-  
   const handleKeyDown = (event) => {
     if (event.key === "Enter") {
       event.preventDefault();
@@ -52,24 +50,27 @@ export default function Fridge() {
     }
   };
 
-  const showButton  = {
-    display: items.length > 0 ? 'block' : 'none'
-    
-  }
-    
+  const showButton = {
+    display: items.length > 0 ? "block" : "none",
+  };
+
   const fetchChatGPTResponse = async (items) => {
     const response = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer `, // Replace with your actual API key
+        Authorization: `Bearer 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`, // Replace with your actual API key
+        organization: "org-2fIccQkIhVpzTF83cBXhZsHF",
       },
       body: JSON.stringify({
         messages: [
-          { role: "system", content: "You: What can I cook with " + items.join(", ") + " only." },
+          {
+            role: "system",
+            content: "You: What can I cook with " + items.join(", ") + " only.",
+          },
           { role: "user", content: "Suggest recipes" },
         ],
-        model: "gpt-3.5-turbo", 
+        model: "gpt-3.5-turbo",
       }),
     });
 
@@ -78,14 +79,20 @@ export default function Fridge() {
     setResponse(message.replace("AI:", "").trim());
   };
 
-  useEffect(() => {submit}, [items]);
-  
-  console.log(response)
+  useEffect(() => {
+    submit;
+  }, [items]);
+
+  console.log(response);
 
   const fridgeItems = items.map((item) => (
     <div key={item} className="fridge__items">
       {item}
-      <button className="fridge__button--delete" type="button" onClick={() => deleteItem(item)}>
+      <button
+        className="fridge__button--delete"
+        type="button"
+        onClick={() => deleteItem(item)}
+      >
         <img className="delete-icon" src={DeleteIcon} alt="" />
       </button>
     </div>
@@ -95,8 +102,12 @@ export default function Fridge() {
     <section>
       <div className="fridge">
         <div className="fridge__scroll-wrapper">
-          <div className="fridge__list">{fridgeItems}</div>
-          {items.length < 1  && <EmptyState IngredientState={true} />}  
+          <div className="fridge__list">
+            {/* Mav: added a title */}
+            <h3>Ingredients</h3> 
+            {fridgeItems}
+          </div>
+          {items.length < 1 && <EmptyState IngredientState={true} />}
         </div>
         <form action="" className="fridge__form">
           <div className="fridge__controls">
@@ -114,14 +125,14 @@ export default function Fridge() {
               onClick={addItem}
               className="fridge__button fridge__button--add"
             >
-            Add
+              Add
             </button>
             <button
               type="button"
               onClick={clearAll}
               className="fridge__button fridge__button--clear"
             >
-            Clear
+              Clear
             </button>
             <button
               style={showButton}
@@ -132,7 +143,6 @@ export default function Fridge() {
               Look for Recipe
             </button>
           </div>
-          
         </form>
       </div>
       {/* <h1>Secret key: {import.meta.env.VITE_OPENAI_KEY}</h1> */}
