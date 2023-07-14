decbe1ea97e9a9bbf692c9d5ebab3f5b8127e55c
Maverick <mavseneris@gmail.com>
2023-07-15 07:59:07 +0800

    fixing deployment bugs

--- src/firebase.js
+++ src/firebase.js
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_REACT_FIREBASE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
