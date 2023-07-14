f8ff17565319cdb874f2edb96861075e6bb9b950
Maverick <mavseneris@gmail.com>
2023-07-15 07:45:19 +0800

    reaname api.js to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,21 +12,3 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
-
-export async function loginUser(creds) {
-  const res = await fetch("/api/login", {
-    method: "post",
-    body: JSON.stringify(creds),
-  });
-  const data = await res.json();
-
-  if (!res.ok) {
-    throw {
-      message: data.message,
-      statusText: res.statusText,
-      status: res.status,
-    };
-  }
-
-  return data;
-}
