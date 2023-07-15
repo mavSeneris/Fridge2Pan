acc653a9018d42ae3f16da83af20200d70ba85a4
choMatt <mattseneris18@gmail.com>
2023-07-15 10:31:39 +0800

    added a back button on error state

--- src/components/contentLoader.jsx
+++ src/components/contentLoader.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 
-export default function contentLoader({isLoading, fridgeView}){
+export default function contentLoader(props){
+  console.log(props)
 
   const fridgeLoadingImg = [
     "https://www.gstatic.com/android/keyboard/emojikitchen/20201001/u1f9d0/u1f9d0_u1f336-ufe0f.png",
@@ -15,7 +16,7 @@ export default function contentLoader({isLoading, fridgeView}){
         <div className="loading-card">
             <div className="loading-text">
               <img
-                src={fridgeView ? fridgeLoadingImg[0] : searchLoadingImg[0]}
+                src={props.fridgeView ? fridgeLoadingImg[0] : searchLoadingImg[0]}
               />
               <h2>Searching Recipe...</h2>
             </div>
@@ -26,16 +27,22 @@ export default function contentLoader({isLoading, fridgeView}){
         <div className="error-card">
             <div className="error-text">
               <img
-                src={fridgeView ? fridgeLoadingImg[1] : searchLoadingImg[1]}
+                src={props.fridgeView ? fridgeLoadingImg[1] : searchLoadingImg[1]}
               />
               {/* <h3>There was an error: {error.message}</h3>; */}
               <h3>Aww... No recipe found :( </h3>
+                <button
+                  className="fridge__button"
+                  onClick={props.back}
+                >
+                  Back
+                </button>
             </div>
           </div>
     )
     return (
         <div>
-            {isLoading ? loadingState : errorState}
+            {props.isLoading ? loadingState : errorState}
         </div>
     )
 }
