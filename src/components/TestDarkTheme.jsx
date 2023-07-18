e3dfabb01709a17524fd233c822ce65c6deed013
Maverick <mavseneris@gmail.com>
2023-07-18 12:00:30 +0800

    implementing darkmode

--- src/components/TestDarkTheme.jsx
+++ src/components/TestDarkTheme.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
-import { DarkModeContext } from "../context/themeContext";
 
 const MyComponent = () => {
   const { isDarkMode, toggleDarkMode } = useOutletContext();
 
   return (
-    <div>
-      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
+    <div className="test">
+      <button onClick={toggleDarkMode}>{!isDarkMode ? "Light Mode" : "Dark Mode"}</button>
       <div className={isDarkMode ? "dark" : "light"}>
         <h1>this is a test page</h1>
       </div>
